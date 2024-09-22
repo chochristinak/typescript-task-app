6d296c76228601edccb757819a2c9be836a23e4d
chock1991 <chochristinak@gmail.com>
2024-09-22 10:51:20 -0600

    Add task select field component

--- src/components/createTaskForm/_taskDateField.tsx
+++ src/components/createTaskForm/_taskDateField.tsx
@@ -1,37 +1,37 @@
-import React, { FC, ReactElement } from 'react';
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
-import { TextField  } from '@mui/material';
-import { IDateField } from './interfaces/IDateField';
-import PropTypes from 'prop-types';
+import React, { FC, ReactElement } from "react";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+import { TextField } from "@mui/material";
+import { IDateField } from "./interfaces/IDateField";
+import PropTypes from "prop-types";
 
+export const TaskDateField: FC<IDateField> = (props): ReactElement => {
+  const {
+    value = new Date(),
+    disabled = false,
+    onChange = (date) => console.log(date),
+  } = props;
+  // State for storing the selected date
 
-export const TaskDateField: FC<IDateField> = (props,): ReactElement => {
-    const { value = new Date(),
-            disabled = false,
-            onChange = (date) => console.log (date)} = props;
-    // State for storing the selected date
-
-    return (
-        <>
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
-            <DesktopDatePicker
-                label="Task Date"
-                format="dd/MM/yyyy" // Correct prop for date format
-                value={value}
-                onChange={onChange}
-                disabled={disabled}
-                views={['year', 'month', 'day']}
-            />
-        </LocalizationProvider>
-        </>
-    );
+  return (
+    <>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <DesktopDatePicker
+          label="Task Date"
+          format="dd/MM/yyyy" // Correct prop for date format
+          value={value}
+          onChange={onChange}
+          disabled={disabled}
+          views={["year", "month", "day"]}
+        />
+      </LocalizationProvider>
+    </>
+  );
 };
 
 TaskDateField.propTypes = {
-    disabled: PropTypes.bool,
-    onChange: PropTypes.func,
-    value: PropTypes.instanceOf (Date),
-}
-
+  disabled: PropTypes.bool,
+  onChange: PropTypes.func,
+  value: PropTypes.instanceOf(Date),
+};
--- src/components/createTaskForm/createTaskForm.tsx
+++ src/components/createTaskForm/createTaskForm.tsx
@@ -1,33 +1,39 @@
-import React, { FC, ReactElement } from 'react';
+import React, { FC, ReactElement } from "react";
 import { Box, Typography, Stack } from "@mui/material";
 
-import { TaskTitleField } from './_taskTitleField';
-import { TaskDescriptionField } from './_taskDescriptionField';
-import { TaskDateField } from './_taskDateField';
+import { TaskTitleField } from "./_taskTitleField";
+import { TaskDescriptionField } from "./_taskDescriptionField";
+import { TaskDateField } from "./_taskDateField";
+import { TaskSelectField } from "./_taskSelectField";
 
 export const CreateTaskForm: FC = (): ReactElement => {
-    return(
-       <Box
-       display="flex"
-       flexDirection="column"
-       alignItems="flex-start"
-       width="100%"
-       px={4}
-       my={6}
-       >
-        <Typography mb={2} component="h2" variant="h6">
-            Create A Task
-        </Typography>
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="flex-start"
+      width="100%"
+      px={4}
+      my={6}
+    >
+      <Typography mb={2} component="h2" variant="h6">
+        Create A Task
+      </Typography>
 
-        <Stack sx={{ width: '100%'}} spacing={2}>
+      <Stack sx={{ width: "100%" }} spacing={2}>
         <TaskTitleField />
         <TaskDescriptionField />
         <TaskDateField />
-        </Stack>
 
-        {/* {//task date}
+        <Stack sx={{ width: "100%" }} direction="row" spacing={2}>
+          <TaskSelectField />
+          <TaskSelectField />
+        </Stack>
+      </Stack>
+
+      {/*
         {//task status}
         {//task priority} */}
-       </Box> 
-    )
+    </Box>
+  );
 };
