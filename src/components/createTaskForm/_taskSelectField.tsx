c72bade21f7c845a3f8f926d4f4d61834ad4c95b
chock1991 <chochristinak@gmail.com>
2024-09-25 08:53:54 -0600

    Update create task form, add task areas

--- src/components/createTaskForm/_taskSelectField.tsx
+++ src/components/createTaskForm/_taskSelectField.tsx
@@ -1,25 +1,59 @@
 import React, { FC, ReactElement } from 'react';
-import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import { ISelectField } from './interfaces/ISelectField';
+import PropTypes from 'prop-types';
 
-export const TaskSelectField: FC = (): ReactElement => {
+export const TaskSelectField: FC<ISelectField> = (
+    props,
+): ReactElement => {
+    // Destructure props
+    const {
+        value = '', 
+        label = 'Select Box', 
+        name = 'selectBox', 
+        items = [{ value: '', label: 'Add Items' }], 
+        disabled = false,
+        onChange = (e: SelectChangeEvent) => console.log(e)
+    } = props;
+
     return (
         <FormControl fullWidth size="small">
-            <InputLabel id="status">Status</InputLabel>
+            <InputLabel id={`${name}-id`}>{label}</InputLabel>
             <Select
-                labelId="status"
-                id="status-select"
-                value=""
-                label="Status"
-                name="status"
+                labelId={`${name}-id`}
+                id={`${name}-id-select`}
+                value={value}
+                label={label}
+                name={name}
+                onChange={onChange}
+                disabled={disabled}
             >
-                <MenuItem value={10}>Ten</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem>
+                {items.map((item, index) => (
+                    <MenuItem
+                        key={item.value + index} 
+                        value={item.value}
+                    >
+                        {item.label}
+                    </MenuItem>
+                ))}
             </Select>
         </FormControl>
     );
+};
+
+
+TaskSelectField.propTypes ={
+    onChange: PropTypes.func,
+    label: PropTypes.string,
+    name: PropTypes.string,
+    disabled: PropTypes.bool,
+    items: PropTypes.arrayOf(
+        PropTypes.shape({
+          value: PropTypes.string.isRequired,
+          label: PropTypes.string.isRequired,
+        }).isRequired,
+    ),
 };
